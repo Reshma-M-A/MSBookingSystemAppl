c46fa08f8e9f03de55e493065d6180d3fe42e38b
sam <sam.grennan8@gmail.com>
2018-03-29 15:01:53 +1100

    added classes

---
+++ src/main/java/com/api/bookingsystem/dto/Entity.java
@@ -0,0 +1,4 @@
+package com.api.bookingsystem.dto;
+
+public class Entity {
+}
---
+++ src/main/java/com/api/bookingsystem/service/BookingService.java
@@ -0,0 +1,7 @@
+package com.api.bookingsystem.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookingService {
+}
