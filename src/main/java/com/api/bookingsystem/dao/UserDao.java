cf07158dbc0e9203791d38eacc3de6670dfbd2a8
clebercassiani <clebercassiani@outlook.com>
2018-03-31 17:15:41 +1100

    added UserService

--- src/main/java/com/api/bookingsystem/dao/UserDao.java
+++ src/main/java/com/api/bookingsystem/dao/UserDao.java
@@ -1,12 +1,14 @@
-package com.api.bookingsystem.service;
+package com.api.bookingsystem.dao;
 
 import java.util.List;
 
 import com.api.bookingsystem.model.SystemUser;
 
-public interface UserService {
+public interface UserDao {
+
     void insertUser(SystemUser user);
     void insertUsers(List<SystemUser> users);
     List<SystemUser> getAllUsers();
-    void getUserByID(long userID);
+    SystemUser getUserById(long userID);
+
 }
--- src/main/java/com/api/bookingsystem/service/UserServiceImpl.java
+++ src/main/java/com/api/bookingsystem/service/UserServiceImpl.java
@@ -1,4 +1,32 @@
 package com.api.bookingsystem.service;
 
-public class UserServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.api.bookingsystem.dao.UserDao;
+import com.api.bookingsystem.model.SystemUser;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+    @Autowired
+    UserDao userDao;
+
+
+    @Override
+    public void insertUser(SystemUser user) { userDao.insertUser(user); }
+
+    @Override
+    public void insertUsers(List<SystemUser> users) { userDao.insertUsers(users); }
+
+    @Override
+    public List<SystemUser> getAllUsers() { return userDao.getAllUsers(); }
+
+    @Override
+    public void getUserByID(long userID) {
+        SystemUser user = userDao.getUserById(userID);
+        System.out.println(user);
+    }
 }
