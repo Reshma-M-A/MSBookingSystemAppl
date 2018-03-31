b429dfbd45284dba23a064e9274b95256f595b08
clebercassiani <clebercassiani@outlook.com>
2018-03-31 17:01:34 +1100

    added dao interface

--- src/main/java/com/api/bookingsystem/dao/UserDaoImpl.java
+++ src/main/java/com/api/bookingsystem/dao/UserDaoImpl.java
@@ -1,4 +1,14 @@
 package com.api.bookingsystem.dao;
 
-public class UserDaoImpl {
+import java.util.List;
+
+import com.api.bookingsystem.model.SystemUser;
+
+public interface UserDaoImpl {
+
+    void insertUser(SystemUser user);
+    void insertUser(List<SystemUser> users);
+    List<SystemUser> getAllEmployees();
+    SystemUser getEmployeeById(String empId);
+
 }
