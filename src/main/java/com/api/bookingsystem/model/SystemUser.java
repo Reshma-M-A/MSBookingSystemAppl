6d0b2dcef4a070b477288e578e4a65a1d5924bc8
clebercassiani <clebercassiani@outlook.com>
2018-03-31 16:57:33 +1100

    Changed SystemUser from Interface to AbstractClass

--- src/main/java/com/api/bookingsystem/model/SystemUser.java
+++ src/main/java/com/api/bookingsystem/model/SystemUser.java
@@ -1,9 +1,15 @@
 package com.api.bookingsystem.model;
 
-public interface SystemUser {
-    public long getUserId();
-    public void setUserId(long empId);
-    public String getUserName();
-    public void setUserName(String empName);
-    public String toString();
+public abstract class SystemUser {
+
+    private long usrID;
+    private String usrName;
+
+    public long getUserId() { return usrID; }
+    public void setUserId(long usrID) { this.usrID = usrID; }
+    public String getUserName() { return usrName; }
+    public void setUserName(String usrName) { this.usrName = usrName; }
+
+    @Override
+    public String toString(){ return "Customer [CustomerID=" + usrID + ", CustomerName=" + usrName + "]"; }
 }
